bba00d836699b81f706dbed1e79fa162a34dd801
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-03 15:10:24 -0800

    created the header

---
+++ final/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div className="heading">
+            <h1>Danika Jaskulak-Gonzalez</h1>
+            <p>FEW 1.2 Final</p>
+        </div>
+    )
+}
+
+export default Header
